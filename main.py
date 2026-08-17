2de5fd69d90c90bcb799d5cbfb5e0c4eea6773e6
qcsoftwaresistema <contato@qcsoftware.com.br>
2026-08-17 15:07:34 -0300

    Update main.py
    Update main qcsoftware.tech.

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ app.mount("/telas", StaticFiles(directory="telas", html=True), name="telas")
 origins = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
+    "https://qcsoftware.tech",
 ]
 
 # Adiciona o middleware de CORS no aplicativo
