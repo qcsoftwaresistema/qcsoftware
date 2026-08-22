fa8908ac8d3a34eb675d8158014b6d16a08a9366
qcsoftwaresistema <contato@qcsoftware.com.br>
2026-08-21 22:50:20 -0300

    Main QCSoftware.tech ok 21082026
    Main QCSoftware.tech ok 21082026

--- main.py
+++ main.py
@@ -29,8 +29,12 @@ app.mount("/telas", StaticFiles(directory="telas", html=True), name="telas")
 
 # Configura as origens permitidas
 origins = [
-    "http://127.0.0.1:5500",
-    "http://localhost:5500",
+        "https://qcsoftware.tech",
+        "https://www.qcsoftware.tech",
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+        "http://127.0.0.1:8000",
+        "http://localhost:8000",
 ]
 
 # Adiciona o middleware de CORS no aplicativo
